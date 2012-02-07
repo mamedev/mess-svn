b3caa8b41764734d1cdd256735c76481f55004c9
curtcoder <curtcoder@9849079f-6229-0410-a07f-84406ce305ba>
2012-02-07 20:46:30 +0000

    [C64] Re-added Skaermtrolden Hugo cartridge support. (nw)

--- src/mess/machine/c64_silverrock.h
+++ src/mess/machine/c64_silverrock.h
@@ -1,6 +1,6 @@
 /**********************************************************************
 
-    Commodore 64 Standard 8K/16K cartridge emulation
+    SilverRock Productions cartridge emulation
 
     Copyright MESS Team.
     Visit http://mamedev.org for licensing and usage restrictions.
@@ -9,13 +9,12 @@
 
 #pragma once
 
-#ifndef __C64_STANDARD_CARTRIDGE__
-#define __C64_STANDARD_CARTRIDGE__
+#ifndef __SILVERROCK__
+#define __SILVERROCK__
 
 #define ADDRESS_MAP_MODERN
 
 #include "emu.h"
-#include "imagedev/cartslot.h"
 #include "machine/c64exp.h"
 
 
@@ -24,33 +23,35 @@
 //  TYPE DEFINITIONS
 //**************************************************************************
 
-// ======================> c64_standard_cartridge_device
+// ======================> c64_silverrock_cartridge_device
 
-class c64_standard_cartridge_device : public device_t,
-									  public device_c64_expansion_card_interface
+class c64_silverrock_cartridge_device : public device_t,
+										public device_c64_expansion_card_interface
 {
 public:
 	// construction/destruction
-	c64_standard_cartridge_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
+	c64_silverrock_cartridge_device(const machine_config &mconfig, const char *tag, device_t *owner, UINT32 clock);
 
 protected:
 	// device-level overrides
-    virtual void device_config_complete() { m_shortname = "c64_std"; }
 	virtual void device_start();
+	virtual void device_reset();
 
 	// device_c64_expansion_card_interface overrides
 	virtual UINT8 c64_cd_r(offs_t offset, int roml, int romh, int io1, int io2);
+	virtual void c64_cd_w(offs_t offset, UINT8 data, int roml, int romh, int io1, int io2);
+	virtual int c64_game_r() { return 0; }
+	virtual int c64_exrom_r() { return 0; }
 	virtual UINT8* c64_roml_pointer(size_t size);
-	virtual UINT8* c64_romh_pointer(size_t size);
 
 private:
 	UINT8 *m_roml;
-	UINT8 *m_romh;
+	UINT8 m_bank;
 };
 
 
 // device type definition
-extern const device_type C64_STD;
+extern const device_type C64_SILVERROCK;
 
 
 #endif
